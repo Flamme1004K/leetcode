37aba882d47a6452895bf028d237f5ff86f04461
Flamme1004K <50702723+Flamme1004K@users.noreply.github.com>
2022-04-21 21:39:40 +0900

    Time: 2 ms (74.39%), Space: 42.2 MB (66.48%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,13 +1,10 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-         if (needle.isEmpty() || haystack.equals(needle)) {
+        if (needle.isEmpty() || haystack.equals(needle) || haystack.startsWith(needle)) {
             return 0;
         }
 
-        if(haystack.contains(needle)) {
-            if(haystack.startsWith(needle)) {
-                return 0;
-            }
+        if (haystack.contains(needle)) {
             String[] split = haystack.split(needle);
             return split[0].length();
         }
